deac17ee9ba371387d392ca077bceff2e31968d0
Luiz <luiz.souza@zup.com.br>
2022-05-25 20:33:50 -0300

    Atualizando...

--- src/main/java/br/com/zup/edu/cadastrodebilhetes/BilheteRequest.java
+++ src/main/java/br/com/zup/edu/cadastrodebilhetes/BilheteRequest.java
@@ -1,5 +1,8 @@
 package br.com.zup.edu.cadastrodebilhetes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import javax.validation.constraints.*;
 
 public class BilheteRequest {
@@ -28,7 +31,8 @@ public class BilheteRequest {
     }
 
     public Bilhete toModel(SorteioRepository sorteioRepository){
-        Sorteio sorteio = sorteioRepository.findById(idSorteio).get();
+        Sorteio sorteio = sorteioRepository.findById(idSorteio).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Id não encontrado!!!"));
         return new Bilhete(nome, celular, sorteio, numero);
     }
 
